0ad112b7c574677a18ad8c64d2f6ced32d437907
tbj <tangbaojie@126.com>
2020-06-01 23:06:07 +0800

    订单接口联调

--- pages/index/order.vue
+++ pages/index/order.vue
@@ -2,48 +2,53 @@
 	<view class="page">
 		<view class="wrapper center-container">
 			<!-- 头部 -->
-			<view class="navbar">
-				<view v-for="(item, index) in navList" :key="index" class="nav-item" :class="{ current: tabCurrentIndex === index }"
-				 @click="tabClick(index)">
-					{{ item.text }}
+			<scroll-view class="wuc-tab" scroll-with-animation scroll-x :scroll-left="scrollLeft" @scroll="scroll">
+				<view class="navbar">
+					<view v-for="(item, index) in navList" :key="index" class="nav-item" :class="{ current: tabCurrentIndex === index }"
+					 @click="tabClick(index)">
+						{{ item.text }}
+					</view>
 				</view>
-			</view>
+			</scroll-view>
 			<!-- 显示区域 -->
 
 			<view class="list" v-for="(item, index) in navList" :key="index" v-if="tabCurrentIndex === index">
 				<div class="listemes" v-for="(item,index) in orderList" :key="index">
 					<div class="content">
 						<div class="tittop">
-							<div class="tittle">{{item.state}}订单：4511441</div>
-							<div class="emesremark c-grey flex"><em>注册手机号:13991186788</em><em class="c-orange">价格:￥14.00</em></div>
+							<div class="tittle">{{item.state}}订单：{{item.orderNo}}</div>
+							<div class="emesremark c-grey flex"><em>注册手机号:{{item.studioName}}</em><em class="c-orange">价格:￥{{item.taskPrice}}</em></div>
 						</div>
 						<ul class="emeslistmark">
-							<li class="flex"> 下单时间：2020-05-20 10:03:33 <em class="c-orange">(持续：24个月)</em></li>
-							<li class="flex"> 手续费：<em class="c-orange">(￥5.00)</em></li>
+							<li class="flex"> 下单时间：{{item.createTime}} </li>
+							<li class="flex"> 手续费：<em class="c-orange">(￥{{item.feePrice}})</em></li>
 							<div class="van-row">
-								<div class="van-col van-col--12">辅助对象：13244567898</div>
-								<div class="van-col van-col--12" style="text-align: right;">是否定向：是</div>
-								<div class="van-col van-col--12">省份：陕西</div>
-								<div class="van-col van-col--12" style="text-align: right;">备注：用户申诉
-								</div>
+								<div class="van-col van-col--12">辅助对象：{{item.helpObj}}</div>
+								<div class="van-col van-col--12" v-if="item.isFix==1" style="text-align: right;">是否定向：是</div>
+								<div class="van-col van-col--12" v-if="item.isFix==0" style="text-align: right;">是否定向：否</div>
+								<div class="van-col van-col--12">省份：{{item.chooseAreaName}}</div>
+
 							</div>
 							<li></li>
 							<li></li>
 						</ul>
-						<div class="tips subject-status-1">等待扫码</div>
+						<view class="tips subject-status-1" v-if="item.orderState==1">等待扫码</view>
+						<view class="tips subject-status-2" v-if="item.orderState==2">正在扫码</view>
+						<view class="tips subject-status-3" v-if="item.orderState==3">等待确认</view>
+						<view class="tips subject-status-4" v-if="item.orderState==4">订单纠纷</view>
+						<view class="tips subject-status-5" v-if="item.orderState==5">订单完成</view>
+						<view class="tips subject-status-1" v-if="item.orderState==6">订单失败</view>
+						<view class="tips subject-status-2" v-if="item.orderState==7">超时退款</view>
 						<!---->
 						<!---->
 					</div>
 					<div class="checkstate">
 						<div class="van-row">
-							<div class="van-col van-col--12 textCenter" @click="enterorder" style="border-right: 1px solid rgb(220, 220, 220);">确认</div>
-							<div class="van-col van-col--12 textCenter">失败</div>
+							<div class="van-col van-col--12 textCenter" @click="enterorder(item.rowId,10)" style="border-right: 1px solid rgb(220, 220, 220);">确认</div>
+							<div class="van-col van-col--12 textCenter" @click="enterorder(item.rowId,20)">失败</div>
 						</div>
 					</div>
 				</div>
-
-
-
 				<view class="loading">{{loadingText}}</view>
 			</view>
 		</view>
@@ -57,30 +62,42 @@
 			return {
 				tabCurrentIndex: 0,
 				page: 1,
+				scrollLeft: 0,
+				oldScrollLeft: 0,
 				orderList: [],
 				navList: [{
 						state: 0,
-						text: '全部',
+						text: '等待扫码',
 						orderList: []
 					},
 					{
 						state: 1,
-						text: '待付款',
+						text: '正在扫码',
 						orderList: []
 					},
 					{
 						state: 2,
-						text: '待收货',
+						text: '等待确认',
 						orderList: []
 					},
 					{
 						state: 3,
-						text: '待评价',
+						text: '订单纠纷',
 						orderList: []
 					},
 					{
 						state: 4,
-						text: '售后',
+						text: '订单完成',
+						orderList: []
+					},
+					{
+						state: 6,
+						text: '订单失败',
+						orderList: []
+					},
+					{
+						state: 7,
+						text: '超时退款',
 						orderList: []
 					}
 				],
@@ -94,7 +111,7 @@
 				console.log('start pulldown');
 			}, 1000);
 			uni.startPullDownRefresh();
-			this.getorderListdata()
+			this.getorderListdata(1)
 		},
 		// 下拉刷新
 		onPullDownRefresh() {
@@ -108,23 +125,39 @@
 		},
 		// 上拉加载
 		onReachBottom: function() {
-				this.getmorenews();
+			this.getmorenews();
 		},
 		methods: {
-			getorderListdata() {
+			//顶部tab点击
+			tabClick(index) {
+				this.page=1;
+				this.tabCurrentIndex =index;
+				this.getorderListdata(this.tabCurrentIndex+1)
+			},
+			getorderListdata(orderId) {
 				let infodata = {
-					orderType: 1,
-					orderState:1
+					orderId:orderId,
+					limit: 10,
+					offset: this.page
 				}
-				util.sendPost('/appOrder/orderList', infodata).then((res) => {
+				util.sendPost('/appOrder/studioOrderList', infodata).then((res) => {
 					console.log(res)
 					if (res.data.code == 0) {
 						uni.showToast({
 							title: res.data.message,
 							icon: 'success',
 						});
-						this.userinfo = res.data.data
-						console.log(this.userinfo)
+
+						this.page++;
+						this.orderList.push(...res.data.data.list);
+						if (res.data.data.list.length == 0) {
+							console.log('已加载全部')
+							uni.hideNavigationBarLoading();
+							this.loadingText = '已加载全部';
+							return false;
+						}
+						uni.hideNavigationBarLoading();
+						// this.orderList = res.data.data.list
 					}
 				}).catch(res => {
 					uni.showToast({
@@ -133,21 +166,34 @@
 					});
 				})
 			},
-			changeTab(e) {
-				this.tabCurrentIndex = e.target.current;
+			scroll: function(e) {
+				this.oldScrollLeft = e.detail.scrollLeft
 			},
-			//顶部tab点击
-			tabClick(index) {
-				this.tabCurrentIndex = index;
-			},
-			enterorder() {
+			
+			
+			enterorder(orderId,type) {
 				uni.showModal({
 					title: '确定要确认订单吗？',
 					success: (res) => {
 						if (res.confirm) {
-							uni.showToast({
-								title: '确认成功',
-								icon: 'success',
+							let btndata = {
+								orderId: orderId,
+								confirmType:type,
+							}
+							util.sendPost('/appOrder/confirmOrder', btndata).then((res) => {
+								console.log(res)
+								if (res.data.code == 0) {
+									uni.showToast({
+										title: res.data.message,
+										icon: 'success',
+									});
+
+								}
+							}).catch(res => {
+								uni.showToast({
+									title: '请求失败',
+									icon: 'none',
+								});
 							})
 						} else if (res.cancel) {
 							uni.showToast({
@@ -166,40 +212,40 @@
 					title: '触底加载成功',
 					icon: 'success',
 				})
-				console.log(this.loadingText)
 				// if (this.loadingText != '' && this.loadingText != '已加载全部') {
 				// 	console.log('return')
 				// 	return false;
 				// }
 				uni.showNavigationBarLoading();
-				let infodata = {
-					fromType: 2,
-					fromPage: 20
-				}
-				util.sendPost('/appUser/getUserInfo', infodata).then((res) => {
-					console.log(res)
-					if (res.data.code == 0) {
-						if (!res.data.data) {
-							uni.hideNavigationBarLoading();
-							this.loadingText = '已加载全部';
-							return false;
-						}
-						this.page++;
-						this.newsList = this.newsList.concat(res.data.split('--hcSplitor--'));
-						this.loadingText = '加载更多';
-						uni.hideNavigationBarLoading();
-					}
-				}).catch(res => {
-					uni.showToast({
-						title: '请求失败',
-						icon: 'none',
-					});
-				})
+				this.getorderListdata()
 			},
 		}
 	};
 </script>
 <style scoped lang="scss">
+	.navbar {
+		display: inline-block !important;
+		width: 100%;
+		height: 45px !important;
+		flex-wrap: wrap;
+		white-space: nowrap;
+		/* 必要，导航栏才能横向*/
+	}
+
+	.navbar .nav-item.current {
+		height: 38px !important;
+	}
+
+	.navbar .nav-item {
+		flex: 0 0 130px !important;
+		height: 40px;
+		margin: 0 10px;
+		line-height: 40px;
+		flex-basis: 130px;
+		display: inline-block !important;
+		/* 必要，导航栏才能横向*/
+	}
+
 	.loading {
 		text-align: center;
 		line-height: 80px;
